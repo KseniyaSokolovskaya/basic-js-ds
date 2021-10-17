8d03b4369eabd158981e6dc7f814b276d2a0fa1d
KseniyaSokolovskaya <deff.1990@gmail.com>
2021-10-17 20:06:46 +0300

    feat: implement method add

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,21 +1,51 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 module.exports = class BinarySearchTree {
-
-  root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.base = null;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  root() {
+    return this.base;
+  }
+
+  add(data) {
+    let node = new Node(data);
+
+    if (this.base === null) {
+      this.base = node;
+      return this;
+    }
+
+    let current = this.base;
+    while (current) {
+      if (node.data === current.data) return undefined;
+
+      if (node.data < current.data) {
+        if (current.left === null) {
+          current.left = node;
+          return this;
+        } else {
+          current = current.left;
+        }
+      } else {
+        if (current.right === null) {
+          current.right = node;
+          return this;
+        } else {
+          current = current.right;
+        }
+      }
+    }
+
+
+
   }
 
   has(/* data */) {
