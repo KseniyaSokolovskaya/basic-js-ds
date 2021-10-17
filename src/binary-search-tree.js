cc496602cf2786f17486004c96bf6dc8c26270b7
KseniyaSokolovskaya <deff.1990@gmail.com>
2021-10-17 23:47:48 +0300

    feat: implement bts

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -67,24 +67,140 @@ module.exports = class BinarySearchTree {
 
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    let node = new Node(data);
+    if (this.base === null) return undefined;
+
+    let current = this.base;
+    let found = false;
+
+    while (current && !found) {
+      if (node.data === current.data) {
+        return current;
+      } else if (node.data < current.data) {
+        current = current.left;
+      } else if (node.data > current.data) {
+        current = current.right;
+      }
+    }
+
+    return null;
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    let left, right;
+
+    if (this.has(data)) {
+      if (this.base.data === data) {
+        left = this.base.left;
+        right = this.base.right;
+        this.base = this.base.right;
+        this.minNode(this.base).left = left;
+        return true;
+      }
+
+      let found = false;
+      let current = this.base;
+      let parent = null;
+
+      while (current && !found) {
+
+        if (data < current.data) {
+          parent = current;
+          current = current.left;
+        } else if (data > current.data) {
+          parent = current;
+          current = current.right;
+        } else {
+
+          if (data > parent.data) {
+            left = current.left;
+            right = current.right
+            if (right) {
+              parent.right = right;
+              this.minNode(right).left = left;
+            }
+            else if (left) {
+              parent.right = left;
+              this.maxNode(left).right = right;
+            } else {
+              parent.right = null
+            }
+          } else {
+            left = current.left;
+            right = current.right
+            if (left) {
+              parent.left = left;
+              this.maxNode(left).right = right;
+            } else if (right) {
+              parent.left = right;
+              this.minNode(right).left = left;
+            } else {
+              parent.left = null;
+            }
+          }
+
+          found = true;
+
+        }
+      }
+    }
+  }
+
+  minNode(from = this.base) {
+    let current = from;
+
+    while (current.left !== null) {
+      current = current.left;
+    }
+    return current;
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+    let current = this.base;
+
+    while (current.left !== null) {
+      current = current.left;
+    }
+    return current.data;
   }
 
+  maxNode(from = this.base) {
+    let current = from;
+
+    while (current.right !== null) {
+      current = current.right;
+    }
+
+    return current;
+  }
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let current = this.base;
+
+    while (current.right !== null) {
+      current = current.right;
+    }
+
+    return current.data;
   }
 
+  toString(node = this.base) {
+    let str = ""
+
+    if (node.left) {
+      str += this.toString(node.left)
+      str += ","
+    }
+
+    str += node.data;
+    str += ","
+
+    if (node.right) {
+      str += this.toString(node.right)
+      str += ","
+    }
+
+    return str
+  }
 }
